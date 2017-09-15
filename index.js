d3b1939919a8ab93894bad6e92ffc3bebc111f44
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-09-15 16:21:32 +0200

    added changes on a layout

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/test";
+const Models = require("./model");
+const models = Models(mongoURL);
+
+
 
 var app = express();
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
   extended: false
 }));
@@ -19,15 +25,22 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
-
-// parse application/x-www-form-urlencoded
-
 app.get('/', function(req, res) {
     res.render('home');
 });
 
+app.post('/', function (req, res) {
+    var regNum = req.body.name;
+    var registrations = "";
+    registrations = regNum;
+      res.render('home', {
+          regNumber: registrations
+      })
+});
+
+
 app.listen(3000);
-// var port = process.env.PORT || 3001;
+// var port = process.env.PORT || 3000;
 // app.listen('port', function() {
 //     console.log("Server running at http://localhost:" + port + "/");
 // })
---
+++ model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+module.exports = function(mongoURL) {
+    mongoose.connect(mongoURL);
+    const regNumbers = mongoose.Schema({
+        name: String
+    })
+    regNumbers.index({
+        name: 1
+    }, {
+        unique: true
+    })
+    const regModel = mongoose.model("regModel", regNumbers)
+
+    return {
+        regModel
+    }
+    // This saves the data to the database
+    Users.save(function(err, results) {
+        if (error) {
+            console.error(error);
+        } else {
+            return results;
+        }
+    });
+}
